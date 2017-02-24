e9f3b4c2252b9f3e285f35a9cadbc9fdf142e92d
YanagiEiichi <admin@web-tinker.com>
2017-02-25 02:15:54 +0800

    Fix coding style

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # geohash.js
+
 "geohash" &lt;=> [lat,lng]
+
+## Install
+
+```bash
+npm install geohash.js
+```
 
 ## Usage
 
 ```javascript
+var lat = 39.90882;
+var lng = 116.39750;
 
- var lat = 39.90882;
- var lng = 116.39750;
- 
- var hash = Geohash.encode(lat, lng); // wx4g09njdr6
- 
- var result = Geohash.decode(hash);
- 
- result[0] === lat; // true
- result[1] === lng; // true
+var hash = Geohash.encode(lat, lng); // wx4g09njdr6
+
+var result = Geohash.decode(hash);
+
+result[0] === lat; // true
+result[1] === lng; // true
 ```
--- geohash.js
+++ geohash.js
@@ -20,25 +20,25 @@ var Geohash = new function() {
 
   // Public methods
   this.encode = function(lat, lng) {
-    // Override 
-    if(lat instanceof Array && lng == null) {
+    // Override
+    if (lat instanceof Array && lng === void 0) {
       lng = lat[1];
       lat = lat[0];
     }
- 
+
     // Type checker
     lat *= 1;
     lng *= 1;
-    if(lat !== lat) throw new TypeError('lat must be a Number');
-    if(lng !== lng) throw new TypeError('lng must be a Number');
-    if(abs(lat) > 90) throw new RangeError('lat must in range [-90,90]');
-    if(abs(lng) > 180) throw new RangeError('lng must in range [-180,180]');
-  
+    if (lat !== lat) throw new TypeError('lat must be a Number');
+    if (lng !== lng) throw new TypeError('lng must be a Number');
+    if (abs(lat) > 90) throw new RangeError('lat must in range [-90,90]');
+    if (abs(lng) > 180) throw new RangeError('lng must in range [-180,180]');
+
     // Compute precision
     var lap = lat.toPrecision(16).match(/\.(.*?)0*$/)[1].length;
     var lop = lng.toPrecision(16).match(/\.(.*?)0*$/)[1].length;
     var prec = pow(10, -max(lap, lop, 0)) / 2;
-     
+
     // Initialize variables
     var rect = new Rect();
     var result = [];
@@ -50,10 +50,10 @@ var Geohash = new function() {
     var last;
 
     // Main loop
-    while(edge >= prec) {
-      if(even) {
+    while (edge >= prec) {
+      if (even) {
         next = rect.halfLng();
-        if(lng === next ? 1 / lng > 1 / next : lng > next) {
+        if (lng === next ? 1 / lng > 1 / next : lng > next) {
           chr |= 1 << bit;
           rect.minlng = next;
         } else {
@@ -61,7 +61,7 @@ var Geohash = new function() {
         }
       } else {
         next = rect.halfLat();
-        if(lat === next ? 1 / lat > 1 / next : lat > next) {
+        if (lat === next ? 1 / lat > 1 / next : lat > next) {
           chr |= 1 << bit;
           rect.minlat = next;
         } else {
@@ -69,12 +69,12 @@ var Geohash = new function() {
         }
       }
       even = !even;
-      if(bit) {
+      if (bit) {
         bit--;
       } else {
         last = edge;
         edge = max(rect.maxlng - rect.minlng, rect.maxlat - rect.minlat);
-        if(last === edge) break;
+        if (last === edge) break;
         result.push(table[chr]);
         bit = 4;
         chr = 0;
@@ -86,9 +86,9 @@ var Geohash = new function() {
   this.decode = function(hash) {
 
     // Type Checker
-    if(!isGeohash.test(hash)) throw new Error('geohash malformed');
+    if (!isGeohash.test(hash)) throw new Error('geohash malformed');
 
-    // Initialize all veriables 
+    // Initialize all veriables
     var rect = new Rect();
     var latE = 90;
     var lngE = 180;
@@ -96,14 +96,14 @@ var Geohash = new function() {
     // Abstract shink operation
     var shink = function(bit, value, even) {
       var bin = 1 << bit;
-      if( !(bit & 1) ^ !(even & 1) ) {
-        if(bin & value) { 
+      if (!(bit & 1) ^ !(even & 1)) {
+        if (bin & value) {
           rect.minlat = rect.halfLat();
         } else {
           rect.maxlat = rect.halfLat();
         }
       } else {
-        if(bin & value) {
+        if (bin & value) {
           rect.minlng = rect.halfLng();
         } else {
           rect.maxlng = rect.halfLng();
@@ -112,11 +112,11 @@ var Geohash = new function() {
     };
 
     // Main loop
-    for (var i = 0,c = hash.length; i < c; i++) {
+    for (var i = 0, c = hash.length; i < c; i++) {
       var value = table.indexOf(hash[i]);
       var even = i & 1;
-      for(var bit = 4; bit >= 0; bit--) shink(bit, value, even);
-      if(even) {
+      for (var bit = 4; bit >= 0; bit--) shink(bit, value, even);
+      if (even) {
         latE /= 8;
         lngE /= 4;
       } else {
@@ -126,8 +126,8 @@ var Geohash = new function() {
     }
 
     // Compute lat/lng and return a round data
-    var pLat = pow(10, max(1, -round( log(latE) / log(10) )) - 1);
-    var pLng = pow(10, max(1, -round( log(lngE) / log(10) )) - 1);
+    var pLat = pow(10, max(1, -round(log(latE) / log(10))) - 1);
+    var pLng = pow(10, max(1, -round(log(lngE) / log(10))) - 1);
     return [
       round(rect.halfLat() * pLat) / pLat,
       round(rect.halfLng() * pLng) / pLng
@@ -137,4 +137,4 @@ var Geohash = new function() {
 
 
 // CMD support
-if(typeof module === 'object') module.exports = Geohash;
+if (typeof module === 'object') module.exports = Geohash;
